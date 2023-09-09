3b9fbe64566da24c41249ed8bd30ec5eca75fd42
AlexNuamah <alexandernuamahjr@gmail.com>
2023-09-09 01:28:13 +0000

    Frontend: Header and Hero Session

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
 
 export const App = () => {
   return (
     <>
-      <h1>My App</h1>
+      <Header />
+      <Outlet />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
+import HomeScreen from './pages/HomeScreen.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>}  />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
